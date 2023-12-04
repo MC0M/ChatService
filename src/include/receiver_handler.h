49434f3560a04496424feb54aed52a9f58d623c4
ng863 <145175503+ng863@users.noreply.github.com>
2023-12-03 22:42:22 -0700

    Add files via upload

---
+++ src/include/receiver_handler.h
@@ -0,0 +1,8 @@
+#ifndef _RECEIVER_HANDLER_H_
+#define _RECEIVER_HANDLER_H_
+#include <dbg.h>
+#include <main.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+void *handle_receiver(void *arg);
+#endif
---
+++ src/include/sender_handler.h
@@ -0,0 +1,7 @@
+#ifndef _SENDER_HANDLER_H_
+#define _SENDER_HANDLER_H_
+#include <main.h>
+#include <sys/socket.h>
+#include <stdio.h>
+void *handle_sender(void *arg);
+#endif
