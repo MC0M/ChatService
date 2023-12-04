a56c624752c262043503ac932c0482bfe00d8bb6
ng863 <145175503+ng863@users.noreply.github.com>
2023-12-03 22:41:46 -0700

    Delete src directory

--- src/include/receiver_handler.h
+++
@@ -1,8 +0,0 @@
-#ifndef _RECEIVER_HANDLER_H_
-#define _RECEIVER_HANDLER_H_
-#include <dbg.h>
-#include <main.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-void *handle_receiver(void *arg);
-#endif
--- src/include/sender_handler.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SENDER_HANDLER_H_
-#define _SENDER_HANDLER_H_
-#include <main.h>
-#include <sys/socket.h>
-#include <stdio.h>
-void *handle_sender(void *arg);
-#endif
